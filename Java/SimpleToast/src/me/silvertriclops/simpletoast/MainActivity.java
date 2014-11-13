5dcb780f099210658df57cbf62171ddc9f292ab2
Jacob Schooley <jacob.schooley@gmail.com>
2014-11-13 10:59:12 -0500

    Click button to show toast

--- Java/SimpleToast/src/me/silvertriclops/simpletoast/MainActivity.java
+++ Java/SimpleToast/src/me/silvertriclops/simpletoast/MainActivity.java
@@ -4,21 +4,29 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
-	@Override
+	@Override	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
 		Context context = getApplicationContext();
-		CharSequence text = "Hello toast!";
+		CharSequence text = context.getString(R.string.toast_notification);
 		int duration = Toast.LENGTH_SHORT;
 		
-		Toast toast = Toast.makeText(context, text, duration);
+		final Toast toast = Toast.makeText(context, text, duration);
 		toast.setGravity(Gravity.TOP|Gravity.LEFT, 0, 0);
-		toast.show();
+		
+		final Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	toast.show();
+            }
+        });
 	}
 }
