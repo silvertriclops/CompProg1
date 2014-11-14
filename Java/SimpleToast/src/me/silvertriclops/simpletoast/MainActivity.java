a32f0a671457eedd54429107f2addc129510e86f
Jacob Schooley <jacob.schooley@gmail.com>
2014-11-14 12:04:44 -0500

    Delete old toast app, create new one and fix appcompat

--- Java/SimpleToast/src/me/silvertriclops/simpletoast/MainActivity.java
+++ Java/SimpleToast/src/me/silvertriclops/simpletoast/MainActivity.java
@@ -4,29 +4,22 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.Gravity;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
+
 
 public class MainActivity extends Activity {
 	
-	@Override	
+	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
 		Context context = getApplicationContext();
-		CharSequence text = context.getString(R.string.toast_notification);
+		CharSequence text = context.getString(R.string.toastMessage);
 		int duration = Toast.LENGTH_SHORT;
 		
-		final Toast toast = Toast.makeText(context, text, duration);
+		Toast toast = Toast.makeText(context, text, duration);
 		toast.setGravity(Gravity.TOP|Gravity.LEFT, 0, 0);
-		
-		final Button button = (Button) findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	toast.show();
-            }
-        });
+		toast.show();
 	}
 }
